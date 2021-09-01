e7a93842520bb5f10da4e9e15c3f8379412af6d0
Anders Byström <andersb84@hotmail.com>
2021-09-01 16:43:53 +0200

    Easypost API key should be added to .env

--- config/account.php
+++ config/account.php
@@ -2,10 +2,6 @@
 
 return [
 
-  // Test
-  'easypost_apikey' => 'EZTKcbb80e8d2b574b219ce3c2e1b8e7fff8zfwSWd9KObxvfxRgOmHLUA'
-  // Prod
-  // 'easypost_apikey' => 'EZAKcbb80e8d2b574b219ce3c2e1b8e7fff823SHMahOzuYs38SNwTq0nQ'
-  // 'easypost_apikey' => env('EASYPOST_API')
+    'easypost_apikey' => env('EASYPOST_API'),
 
 ];
