a9b23a5eb462a0fe12ebe26647ca794e7b1e09b5
jkread <jr@johnkread.com>
2021-09-07 15:33:00 -0500

    offers to order

--- resources/views/customer/bundles/edit.blade.php
+++ resources/views/customer/bundles/edit.blade.php
@@ -5,12 +5,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h2><i class="nav-icon fas fa-shopping-basket"></i> My Offers</h2>
+                        <h2><i class="nav-icon fas fa-shopping-basket"></i> My Orders</h2>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ url('customer/dashboard') }}" class="fontGray1"><i class="nav-icon fas fa-tachometer-alt"></i> Dashboard</a></li>
-                            <li class="breadcrumb-item"><a href="{{ url ('customer/my-bundles') }}" class="fontGray1"><i class="nav-icon fas fa-shopping-basket"></i> My Bundles</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url ('customer/my-bundles') }}" class="fontGray1"><i class="nav-icon fas fa-shopping-basket"></i> My Orders</a></li>
                             <li class="breadcrumb-item active">Details</li>
                         </ol>
                     </div>
